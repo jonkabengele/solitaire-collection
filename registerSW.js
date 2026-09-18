18210c055bdf8adc97d364f34ce9b0600acfc598
Jonathan <jonkabengele@gmail.com>
2026-09-18 13:27:49 +0200

    Deploy bigger card typography

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/solitaire-collection/sw.js', { scope: '/solitaire-collection/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
